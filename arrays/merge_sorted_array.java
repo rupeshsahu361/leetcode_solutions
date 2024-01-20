0583ecf007a41b4cb5e4d6b8f295b8e637d99284
rupeshsahu361 <31150572+rupeshsahu361@users.noreply.github.com>
2024-01-20 09:20:28 +0530

    Create merge_sorted_array.java

---
+++ arrays/merge_sorted_array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        m--;
+        n--;
+        while(m>=0 && n>=0) {
+            if (nums1[m] > nums2[n]) {
+                nums1[m+n+1] = nums1[m];
+                m--;
+            } else {
+                nums1[m+n+1] = nums2[n];
+                n--;
+            }
+        }
+        while (m >= 0) {
+            nums1[m+n+1] = nums1[m];
+            m--;
+        }
+        while (n >= 0) {
+            nums1[m+n+1] = nums2[n];
+            n--;
+        }
+    }
+}
