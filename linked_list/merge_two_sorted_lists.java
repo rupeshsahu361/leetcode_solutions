711fe811d9dc302c5b536e7377d940469d8f8f17
rupeshsahu361 <31150572+rupeshsahu361@users.noreply.github.com>
2024-01-26 17:27:46 +0530

    Create merge_two_sorted_lists.java

---
+++ linked_list/merge_two_sorted_lists.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode(0);
+        ListNode current = head;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+        while (list1 != null) {
+            current.next = list1;
+            list1 = list1.next;
+            current = current.next;
+        }
+        while (list2 != null) {
+            current.next = list2;
+            list2 = list2.next;
+            current = current.next;
+        }
+        return head.next;
+    }
+}
