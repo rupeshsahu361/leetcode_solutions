0f9594c54e929bcbf06426995b3f9c1f5032fb42
rupeshsahu361 <31150572+rupeshsahu361@users.noreply.github.com>
2024-02-13 17:13:27 +0530

    Create subsets_II.java

---
+++ recursion/subsets_II.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        findSubsets(0, nums, new ArrayList<>(), ans);
+        return ans;
+    }
+    public void findSubsets(int index, int[] nums, List<Integer> ds, List<List<Integer>> ans) {
+        ans.add(new ArrayList<>(ds));
+        for (int i=index; i<nums.length; i++) {
+            if (i!=index && nums[i]==nums[i-1]) {
+                continue;
+            }
+            ds.add(nums[i]);
+            findSubsets(i+1, nums, ds, ans);
+            ds.remove(ds.size()-1);
+        }
+    }
+}
